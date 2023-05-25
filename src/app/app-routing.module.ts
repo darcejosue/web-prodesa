6bf6e260d2e4c251f05a8b6bdc0d0e56cac8792d
Josue Darce <darcejosue30@gmail.com>
2023-05-25 07:59:45 -0600

    initial commit to web prodesa

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MainContentComponent } from './pages/main-content/main-content.component';
+import { SucursalesComponent } from './pages/sucursales/sucursales.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {
+    path:'',
+    component: MainContentComponent 
+  },
+  {
+    path:'home',
+    component: SucursalesComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderTopComponent } from './pages/header-top/header-top.component';
+import { FooterBottomComponent } from './pages/footer-bottom/footer-bottom.component';
+import { MainContentComponent } from './pages/main-content/main-content.component';
+import { SucursalesComponent } from './pages/sucursales/sucursales.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderTopComponent,
+    FooterBottomComponent,
+    MainContentComponent,
+    SucursalesComponent
   ],
   imports: [
     BrowserModule,
