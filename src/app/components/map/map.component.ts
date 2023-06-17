aa603460e79aeef301087678135a2609a9bc6dbc
Josue Darce <darcejosue30@gmail.com>
2023-06-17 12:57:19 -0600

    Mapa de las agencias

--- src/app/components/map/map.component.ts
+++ src/app/components/map/map.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./map.component.css']
 })
 export class MapComponent {
+  
+  juigalpa(){
+    let ubicacion = "https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=4J4M+4W7,%20NIC-7,%20Juigalpa+(Prodesa)&amp;t=p&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed";
+          
+  }
 
 }
+
